29e37a4759a96313251cb8be1e5f9320daa1360f
Keith LePoidevin <klepoidevin3@gmail.com>
2024-05-11 17:10:00 -0700

    Added auth
    Can create user, login, logout, added redirect to original URL on login.

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const mongoose = require('mongoose');
 const Bet = require('./models/bet');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
+const { isLoggedIn } = require('./middleware');
+const { storeReturnTo } = require('./middleware');
 
 mongoose.connect('mongodb://127.0.0.1:27017/betApp')
     .then(() => {
@@ -21,13 +26,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 const PORT = process.env.PORT || 3000;
 
-// Static Files
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use(express.urlencoded({ extended: true }));
-
-
 const sessionConfig = {
     secret: 'secretgoeshere',
     resave: false,
@@ -38,12 +36,26 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-app.use(session(sessionConfig));
 
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.session());
+app.use(passport.initialize());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
-    res.locals.messages = req.flash('success');
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     next();
 })
 
@@ -52,17 +64,52 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/bets', async (req, res) => {
+app.get('/bets', isLoggedIn, async (req, res) => {
     const bets = await Bet.find({});
     res.render('bets', { bets });
 });
 
-app.post('/bets', async (req, res) => {
+app.post('/', isLoggedIn, async (req, res) => {
     const newBet = new Bet(req.body);
     await newBet.save();
     req.flash('success', 'New bet saved');
     res.redirect('/');
 })
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err => {
+        if (err) return next(err);
+        req.flash('success', 'Your account has been created, and you are logged in');
+        res.redirect('/bets');
+    })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'You are logged in');
+    const redirectURL = res.locals.returnTo;
+    res.redirect(redirectURL);
+});
+
+app.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/login');
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}...`)
